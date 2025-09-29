5020c20ae5f0ae826cb3a964dfac31433e9996fd
Utkarsh-Awasthi08 <utkarsha070@gmail.com>
2025-09-29 23:48:16 +0530

    Added backend uri

--- src/config/AxiosHelper.js
+++ src/config/AxiosHelper.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-export const baseURL = "https://chat-app-backend-2-3hez.onrender.com/";
+export const baseURL = "https://chat-app-backend-3-lgwt.onrender.com/";
 export const httpClient = axios.create({
   baseURL: baseURL,
 });
